6f399e06bcfc4b9b6a6fb5196b0a426b880d74bb
cand1992 <christopher_anderson@mac.com>
2013-10-14 19:01:27 -0600

    Adding Updating time in the home.html.erb body. adding jumbotron homepage as well.

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,19 @@
+var serverTime = new Date();
+
+function updateTime() {
+    /// Increment serverTime by 1 second and update the html for '#time'
+    serverTime = new Date(serverTime.getTime() + 1000);
+    
+    var curr_hour = serverTime.getHours();
+    var curr_min = serverTime.getMinutes();
+    var curr_sec = serverTime.getSeconds();
+    $('#time').html(curr_hour + ':' + curr_min + ':' + curr_sec);
+
+
+
+}
+
+$(function() {
+    updateTime();
+    setInterval(updateTime, 1000);
+});
