12da78544167244cf699b3f1e099a382ea91d4b9
cand1992 <christopher_anderson@mac.com>
2013-10-16 12:57:52 -0600

    Adds working javascript clock to homepage.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,19 +1,36 @@
-var serverTime = new Date();
+// var serverTime = new Date();
 
-function updateTime() {
-    /// Increment serverTime by 1 second and update the html for '#time'
-    serverTime = new Date(serverTime.getTime() + 1000);
+// function updateTime() {
+//     /// Increment serverTime by 1 second and update the html for '#time'
+//     serverTime = new Date(serverTime.getTime() + 1000);
     
-    var curr_hour = serverTime.getHours();
-    var curr_min = serverTime.getMinutes();
-    var curr_sec = serverTime.getSeconds();
-    $('#time').html(curr_hour + ':' + curr_min + ':' + curr_sec);
+//     var curr_hour = serverTime.getHours();
+//     var curr_min = serverTime.getMinutes();
+//     var curr_sec = serverTime.getSeconds();
+//     $('#time').html(curr_hour + ':' + curr_min + ':' + curr_sec);
 
 
 
+// }
+
+// $(function() {
+//     updateTime();
+//     setInterval(updateTime, 1000);
+// });
+
+function updateTime(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('time_span').innerHTML = t_str;
 }
-
-$(function() {
-    updateTime();
-    setInterval(updateTime, 1000);
-});
+setInterval(updateTime, 1000);
