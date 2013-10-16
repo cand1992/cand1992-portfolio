afb70694b892955301e2d2c39e8e489b017b8d8b
cand1992 <christopher_anderson@mac.com>
2013-10-16 13:52:38 -0600

    Adds working sign up and sign in links.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -25,12 +25,9 @@ function updateTime(){
     if (minutes < 10){
         minutes = "0" + minutes
     }
-    var t_str = hours + ":" + minutes + " ";
-    if(hours > 11){
-        t_str += "PM";
-    } else {
-        t_str += "AM";
-    }
+    var t_str = hours + ":" + minutes;  
+    
+    
     document.getElementById('time_span').innerHTML = t_str;
 }
 setInterval(updateTime, 1000);
