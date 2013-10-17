338b7513899880482f0f8689b3e43a4a1f1f2ab6
cand1992 <christopher_anderson@mac.com>
2013-10-16 18:46:54 -0600

    adds email layouts and sets them to every email sent from app.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@gmail.com"
 
+  layout 'email'
+
   def welcome(user)
     @user = user
     mail(to: @user.email, subject: "Welcome to Chris's portfolio!")
